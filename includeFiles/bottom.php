07ec9b34505fa1ba6e8ef9ba83e0f55b2000a37f
Jane <Jane@Jane-PC>
2015-04-25 20:48:13 +0800

    新增config

--- includeFiles/bottom.php
+++ includeFiles/bottom.php
@@ -1,11 +1,8 @@
-    </body>
+<div class="bottom">
+    <p>友情链接</p><br>    
+     <p> 闽ICP备XXXXX号
+©XXXX.com Inc. All Rights Reserved </p>
+</div> 
+</body>
 </html>
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
 
--- includeFiles/top.php
+++ includeFiles/top.php
@@ -1,10 +1,15 @@
+<?php
+include 'includeFiles/config.php';
+?>
 <html>
     <head>
          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+         <title>Jimmy博客</title>
     </head>
     <body>
+        <div>            
         <a href="/">首页</a>|<a href="#">文章</a>
-
+        </div>
 <?php
 
 /* 
