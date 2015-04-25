270f067099a49fae1a05a11ba2f8225510e06c45
Jimmy <hltzba@163.com>
2015-04-25 23:15:55 +0800

    测试文件

--- includeFiles/top.php
+++ includeFiles/top.php
@@ -10,12 +10,3 @@ include 'includeFiles/config.php';
         <div>            
         <a href="/">首页</a>|<a href="#">文章</a>
         </div>
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-?>
