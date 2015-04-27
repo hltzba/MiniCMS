06723e66e39b3587903c11041d0793895a6537cf
Jimmy <hltzba@163.com>
2015-04-28 00:00:37 +0800

    临时提交

--- includeFiles/top.php
+++ includeFiles/top.php
@@ -12,6 +12,11 @@ include 'includeFiles/config.php';
         <div>            
             <a href="/">首页</a> | <a href="/news">文章</a>
             | <a href="/userreg">注册</a>
+            <?php
+                     include 'module/webuser.php';
+                     $user=new WebUser();
+                     echo $user->CurrentUserName;
+            ?>
         </div>
         <?php 
         //加载模板
---
+++ module/webuser.php
@@ -0,0 +1,10 @@
+<?php
+    ///用户处理类
+    class WebUser
+    {
+        public  $CurrentUserName='游客';
+                function __construct() {
+            //echo '用户已经初始化';
+        }
+    }
+?>
