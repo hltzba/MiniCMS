deec86af3bc916472ff0a4b662ad6bc7bd3da6b6
Jane <Jane@Jane-PC>
2015-04-28 21:49:58 +0800

    完善webuser类

--- includeFiles/top.php
+++ includeFiles/top.php
@@ -12,11 +12,14 @@ include 'includeFiles/config.php';
         <div>            
             <a href="/">首页</a> | <a href="/news">文章</a>
             | <a href="/userreg">注册</a>
+            <span style="color:green">
             <?php
                      include 'module/webuser.php';
-                     $user=new WebUser();
-                     echo $user->CurrentUserName;
+                 
+                     echo '欢迎 '.WebUser::GetCurrentUser();
             ?>
+            </span>
+            <span><a href="#">退出登录</a></span>
         </div>
         <?php 
         //加载模板
--- module/webuser.php
+++ module/webuser.php
@@ -2,9 +2,42 @@
     ///用户处理类
     class WebUser
     {
-        public  $CurrentUserName='游客';
+//        public  $CurrentUserName='游客';
+//        public $userAlias;
+        private $UserInfo=array();
+        
                 function __construct() {
             //echo '用户已经初始化';
+        }
+        
+        //获取用户类属性
+        function __get($name) {
+            if(array_key_exists($name, $UserInfo))
+            {
+                return $UserInfo[$name];
+            }
+            else
+            {
+                return "";
+            }
+        }
+        
+        //设置用户类属性
+        function __set($name, $value) {
+          $this->UserInfo[$name]=$value;
+        }
+        
+        function __call($name, $arguments) {
+            switch ($name)
+            {
+                case "add":
+                {}break;
+            }
+        }
+        
+       public static function GetCurrentUser()
+        {
+           echo 'Jimmy';
         }
     }
 ?>
