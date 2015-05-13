72227dfc485d7d75ae0c5af2407b93b3fa2fb5ad
Jimmy <hltzba@163.com>
2015-05-13 23:05:17 +0800

    通用头部OK

--- includeFiles/top.php
+++ includeFiles/top.php
@@ -8,30 +8,46 @@ include 'includeFiles/config.php';
          echo $CurrentTitle;
          ?></title>
          <style>
-             .container{background-color: #474747;height: 130px;margin: 0 auto;}
-             .container a{color:#fff;}
+             *{margin: 0 auto;text-align: center;font-size: 14px;}
+             body{background: url(/image/bg.png)}
+             .container{background-color: #474747;height: 130px;margin: 0 auto;
+             color:#fff;}
+             .container a{color:#fff;text-decoration: none;}
+             .container a:hover{text-decoration: underline;}
+             .container .top{width: 1000px;}
+             .container .logo{float: left;}
+             .container .menu{width: 400px;float: left;margin-top:30px;
+                  margin-left: 15px;text-align: left;}
+             .container .userinfo{float: right;text-align: right;}
+             .container .search{border:solid 1px gray;width:150px;
+                    text-align: left;height: 21px;line-height: 21px;
+                    background: url(/image/topsearch.gif) no-repeat #fff right;}
           </style>
     </head>
     <body>
         <div class="container"><!--容器DIV-->
             <div class="top">
                 <div class="logo">
-                    <a href="/"><img src="/image/logo.png" alt="Jimmy and Jane 's Sweet"/></a>
+                    <a href="/"><img src="/image/logo.gif" alt="Jimmy and Jane 's Sweet"/></a>
                 </div>
-                <div class="menu">
-                    <a href="/">首页</a> | <a href="/news">文章</a>
-            | <a href="/userreg">注册</a>
-                </div>
+                
                 <div class="userinfo">
-                       <span>
-            <?php
-                     include 'module/webuser.php';
-                 
-                     echo '欢迎 '.WebUser::GetCurrentUser();
-            ?>
-            </span>
+                   <span>
+                        <?php
+                            include 'module/webuser.php';                 
+                            echo '欢迎回来 '.WebUser::GetCurrentUser();
+                        ?>
+                 </span>
             <span><a href="#">退出登录</a></span>
                 </div>
+                
+                
+                <div class="menu">
+                    <a href="/">首页</a> | <a href="/news">文章</a>
+            |       <a href="/userreg">注册</a>
+                    <input type="text" class="search"/>
+                </div>
+          
             </div>
         </div>
         
