4c2e9ca0469a8ecb375b29a420231ff5b0073fd0
Jimmy <hltzba@163.com>
2015-05-21 23:56:12 +0800

    首页新闻列表的UI完善 新闻业务逻辑虚代码概要编写

--- tpl/index.php
+++ tpl/index.php
@@ -4,12 +4,17 @@
     .content .right{float: right;width: 29%}
     .content .left{float: left;width: 69%;}
     .newsregion{
-        color:#9c9c9c;width: 100%;background: #fff;float: left;margin: 5px auto;border-radius: 10px;border: solid 1px grey;
+        color:#9c9c9c;width: 100%;background: #fff;float: left;margin: 5px auto;border-radius: 10px;border: solid 1px #ddd;
     }
    .newsregion dt{
-       color:#383838;width: 95%;line-height: 20px;border-bottom: solid 1px grey;text-align: left;padding: 6px;
+       color:#383838;width: 95%;line-height: 20px;border-bottom: solid 1px #ddd;text-align: left;padding: 6px;
    }
-   .newsregion dd{width: 95%;line-height: 20px;border-bottom: solid 1px grey;text-align: left;padding: 10px;text-indent:2en;}
+   .newsregion dd{width: 95%;line-height: 20px;text-align: left;padding: 10px;text-indent:2en;}
+   .newsregion .pubinfo
+   {
+       border: 0;background: url(/image/newsbar.jpg);width: 98%;height: 25px;
+       border-top: solid 1px #ddd;padding:10px 0px 5px 10px; margin-bottom: 1px; 
+   }
 </style>
 <div class="indexContainer">
     <div class="content">
@@ -22,14 +27,14 @@
 
 马化腾在前言中提出，“互联网+”，是一种“寓大于小”的生态战略。在万物互联的新生态中，企业不再是社会经济活动的最小单位，个人才是社会经济活动的最小细胞。这使得传统企业的形态、边界正在发生变化，开放、灵活、“寓大于小”成为商业变革的趋势。
                 </dd>
-                <dd>腾讯财经 2015-05-19 14:14</dd>
+                <dd class="pubinfo">腾讯财经 2015-05-19 14:14</dd>
             </dl>
             
             <dl class="newsregion">
                 <dt>奥巴马开通Twitter帐号 用的是iPhone手机</dt>
                 <dd>美国总统巴拉克·奥巴马终于开通自己的Twitter帐号了！周一上午（编注：美国东部时间），奥巴马发出了自己的第一条Twitter消息，称“你好，Twitter！这是巴拉克·奥巴马！真的！6年时间了，他们终于给了我自己的帐号。”而且，奥巴马此番还是使用iPhone发出了自己的第一条Twitter消息。
                 </dd>
-                <dd>腾讯科技 2015年05月19日06:41</dd>
+                <dd class="pubinfo">腾讯科技 2015年05月19日06:41</dd>
             </dl>
         </div>
         <div class="right">
